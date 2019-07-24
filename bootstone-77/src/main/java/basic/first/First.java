d93e8fa8f7f8838817188680df2e6e04067c9d2c
lsh <522097606@qq.com>
2019-07-24 16:52:02 +0800

    love 77

--- bootstone-77/src/main/java/basic/first/First.java
+++ bootstone-77/src/main/java/basic/first/First.java
@@ -16,12 +16,15 @@ package basic.first;
  */
 public class First {
 
-    /**
-     * 一个类的主方法
-     * @param args
-     */
-    public static void main(String[] args) {
 
+    /** 这是第一个Java程序
+     *它将打印Hello World
+     * 这是一个多行注释的示例
+     */
+    public static void main(String []args){
+        // 这是单行注释的示例
+        /* 这个也是单行注释的示例 */
+        System.out.println("Hello World");
     }
 
 }
