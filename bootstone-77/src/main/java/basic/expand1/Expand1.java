97ba66f4ee8a358e57a0064caf361d0ef0cafd48
lsh <522097606@qq.com>
2019-07-25 16:30:48 +0800

    love 77

---
+++ bootstone-77/src/main/java/basic/expand1/Expand1.java
@@ -0,0 +1,32 @@
+package basic.expand1;
+
+/**
+ * Created by lsh on 2019-07-25.
+ */
+public class Expand1 {
+
+    public static void main(String[] args) {
+        /**
+         * 用true和false分别判断身高1米和1.5米的儿童乘坐火车时是否应该购票（超过1.2米的孩童乘坐火车时需购票）
+         */
+        //小李身高1米，小明身高1.5米
+        double xiaoli = 1;
+        double xiaoliu = 1.5;
+        // 调用buyTicket方法，传入小李和小刘身高
+        System.out.println("小李要不要买票 ： " + buyTicket(xiaoli));
+        System.out.println("小刘要不要买票 ： " + buyTicket(xiaoliu));
+    }
+
+    /**
+     * 思路 ： 该方法接收参数为 身高，double类型，返回值是boolean，true代码身高大于1.2，即需要买票
+     * @return 布尔值
+     */
+    public static boolean buyTicket(double x){
+        if(x > 1.2){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+}
