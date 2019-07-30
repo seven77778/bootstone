bfaca8c0d66c7bcb9305b18858d6a4a4b8e52335
lsh <522097606@qq.com>
2019-07-30 16:33:24 +0800

    love 77

---
+++ bootstone-77/src/main/java/basic/eighth/Game1.java
@@ -0,0 +1,26 @@
+package basic.eighth;
+
+import java.util.Scanner;
+
+/**
+ * Created by lsh on 2019-07-30.
+ */
+public class Game1 {
+
+    /**
+     * 猜数字游戏：假使目标数字是147，使用while循环实现控制台的多次输入，才对后终止程序
+     * @param args
+     */
+    public static void main(String[] args) {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请在下一行输入数字");
+            int x = scanner.nextInt();
+            if(x == 147){
+                System.out.println("恭喜你猜对了，答案是147");
+                break;
+            }
+            System.out.println("没猜到，请继续");
+        }
+    }
+}
