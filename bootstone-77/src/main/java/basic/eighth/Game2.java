1ca726b3908568987db6495c034bf808dcb52a8f
lsh <522097606@qq.com>
2019-07-30 16:41:35 +0800

    love 77

---
+++ bootstone-77/src/main/java/basic/eighth/Game2.java
@@ -0,0 +1,29 @@
+package basic.eighth;
+
+/**
+ * Created by lsh on 2019-07-30.
+ */
+public class Game2 {
+
+    /**
+     * 生物实验室 细菌繁殖试验
+     * 第一代 1 个
+     * 第二代 2个
+     * 第三代 4个
+     * 以此类推的话，
+     * 第四代 8个
+     * 第五代 16个，好像每次都是乘以2
+     *
+     * 思路，循环个10次，乘以2
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int cellNumbers = 1;//第一代细菌
+        for (int i =0;i<9;i++){ //上面是第一代，在循环9次，就是第十代了吧
+            cellNumbers = cellNumbers*2;//每一代都是上一代的2倍
+        }
+        System.out.println("第十代细菌 是 " + cellNumbers);
+    }
+
+}
