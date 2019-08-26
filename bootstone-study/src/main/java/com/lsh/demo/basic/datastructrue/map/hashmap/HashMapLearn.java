69b671c89a63dc7cc82dd277f76112b34637fe31
lsh <522097606@qq.com>
2019-08-26 11:00:50 +0800

    love77

--- bootstone-study/src/main/java/com/lsh/demo/basic/datastructrue/map/hashmap/HashMapLearn.java
+++ bootstone-study/src/main/java/com/lsh/demo/basic/datastructrue/map/hashmap/HashMapLearn.java
@@ -17,7 +17,11 @@ import java.util.concurrent.ConcurrentHashMap;
  * 4.重写tostring  return key + "=" + value;
  * 5.重写 hashcode Objects.hashCode(key) ^ Objects.hashCode(value)
  *
+ *  因为多线程环境下，使用Hashmap进行put操作会引起死循环，导致CPU利用率接近100%，所以在并发情况下不能使用HashMap。
  *
+ *  HashTable容器使用synchronized来保证线程安全，但在线程竞争激烈的情况下HashTable的效率非常低下。
+ *  因为当一个线程访问HashTable的同步方法时，其他线程访问HashTable的同步方法时，可能会进入阻塞或轮询状态。
+ *  如线程1使用put进行添加元素，线程2不但不能使用put方法添加元素，并且也不能使用get方法来获取元素，所以竞争越激烈效率越低。
  *
  */
 public class HashMapLearn {
