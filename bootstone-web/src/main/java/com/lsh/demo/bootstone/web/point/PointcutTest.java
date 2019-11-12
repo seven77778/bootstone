2273f7f5a08f31f00a71d10ca187e9aad0b82e12
lsh <522097606@qq.com>
2019-11-12 17:15:38 +0800

    point可以自定义名称

--- bootstone-web/src/main/java/com/lsh/demo/bootstone/web/point/PointcutTest.java
+++ bootstone-web/src/main/java/com/lsh/demo/bootstone/web/point/PointcutTest.java
@@ -10,44 +10,51 @@ import java.util.List;
 
 /**
  * Created by lsh on 2019-11-08.
+ *
+ * 1. around 需要有  point.proceed() + 返回值
+ * 2. @Pointcut 可以有多个
+ *
  */
 @Aspect
 @Component
 public class PointcutTest {
 
-    @Pointcut("execution(* com.lsh.demo.bootstone.web.controller.HelloController.test2(..))")
+    @Pointcut("execution(* com.lsh.demo.bootstone.web.controller.HelloController.test(..))")
     public void pointCut() {
         System.out.println("aaaa");
     }
 
+    @Pointcut("execution(* com.lsh.demo.bootstone.web.controller.HelloController.test2(..))")
+    public void pointCut2() {
+        System.out.println("aaaa");
+    }
 
-//    @AfterReturning(value = "execution(* com.lsh.demo.bootstone.web.controller.HelloController.test2(..))", returning = "baseResponse")
-//    public void after(JoinPoint joinPoint, String baseResponse) {
-//
-//        System.out.println("after" + baseResponse);
-//    }
+    @AfterReturning(pointcut = "pointCut2()", returning = "baseResponse")
+    public void after(JoinPoint joinPoint, String baseResponse) {
 
-    @Before("execution(* com.lsh.demo.bootstone.web.controller.HelloController.test2(..))")
+        System.out.println("after" + baseResponse);
+    }
+
+    @Before("pointCut2()" )
     public void before(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
         System.out.println("before");
     }
 
-    @After(("execution(* com.lsh.demo.bootstone.web.controller.HelloController.test2(..))"))
+    @After("pointCut2()")
     public void afterMethod(JoinPoint point) {
         String methodName = point.getSignature().getName();
         List<Object> args = Arrays.asList(point.getArgs());
         System.out.println("After：" + methodName + ",参数为：" + args);
     }
 
-    @Around(("execution(* com.lsh.demo.bootstone.web.controller.HelloController.test2(..))"))
-    public void around(ProceedingJoinPoint point) throws Throwable {
+    @Around("pointCut2()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
         String methodName = point.getSignature().getName();
         List<Object> args = Arrays.asList(point.getArgs());
-        point.proceed();
-
-        System.out.println("around：" + methodName + ",参数为：" + args);
+        System.out.println("around：" + methodName + ",参数为：" + args );
+        return point.proceed();
     }
 
 }
