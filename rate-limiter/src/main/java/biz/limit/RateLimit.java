ed8cd5592ac1f79f882748f85e0b04f806903f8e
seven77778 <522097606@qq.com>
2020-11-05 18:09:54 +0800

    add

--- rate-limiter/src/main/java/biz/limit/RateLimit.java
+++ rate-limiter/src/main/java/biz/limit/RateLimit.java
@@ -4,6 +4,11 @@ package biz.limit;
  * Created by lsh on 2020-09-23.
  *
  *  限流降级
+ *
+ *  升级理念
+ *   // >100us, use mutex lock
+ *     // >15us, use yield, just context switch
+ *     // <15us, use spin
  */
 public interface RateLimit {
 
